b4475bdcf1683349a48257564fd2627ce80203c6
EloM28 <mugisheloge@gmail.com>
2025-03-19 18:56:25 +0200

    Added the consumer for the asgi

---
+++ chat/consumers.py
@@ -0,0 +1,17 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import Room, Message
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.room_name = f"room_{self.scope['url_route']['kwargs']['room_name']}"
+        await self.channel_layer.group_add(self.room_name, self.channel_name)
+
+        await self.accept()
+
+    async def disconnect(self, code):
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        self.close(code)
